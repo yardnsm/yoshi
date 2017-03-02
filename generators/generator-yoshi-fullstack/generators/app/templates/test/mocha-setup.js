e225735ed71299999b5e6253a605f8c59dc63782
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-03-02 17:49:31 +0200

    update generators

--- generators/generator-yoshi-fullstack/generators/app/templates/test/mocha-setup.js
+++ generators/generator-yoshi-fullstack/generators/app/templates/test/mocha-setup.js
@@ -1,6 +1,1 @@
 import 'babel-polyfill';
-import axios from 'axios';
-import {wixAxiosConfig} from 'wix-axios-config';
-import {getTestBaseUrl} from './test-common';
-
-wixAxiosConfig(axios, {baseURL: getTestBaseUrl()});
