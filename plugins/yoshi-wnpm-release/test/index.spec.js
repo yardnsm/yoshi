8e6afab9f38df0d6173ff6f1dc5721dd0a3b0d4e
Ronen Amiel <ronen.amiel@gmail.com>
2017-03-22 17:29:30 +0200

    yoshi-wnpm-release

---
+++ plugins/yoshi-wnpm-release/test/index.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const {expect} = require('chai');
+// const tp = require('test-phases');
+// const wnpmRelease = require('../index');
+
+describe('wnpm release', () => {
+  // let test;
+  // let task;
+
+  // beforeEach(() => test = tp.create());
+  // beforeEach(() => process.chdir(test.tmp));
+  // beforeEach(() => task = wnpmRelease({log: a => a}));
+  // afterEach(() => test.teardown());
+
+  // TODO: write tests
+});
