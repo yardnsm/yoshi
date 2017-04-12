9a85e5770f98d3647be84c8e1ccb24ceb1897050
Ronen Amiel <ronen.amiel@gmail.com>
2017-04-12 15:40:38 +0300

    support running tasks by reference

--- yoshi/lib/run.js
+++ yoshi/lib/run.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const {isString} = require('lodash');
 const {watchMode, logIfAny, inTeamCity} = require('./utils');
 const {log, logIf, logIfP} = require('./log');
 const {base, statics} = require('./globs');
+const projectConfig = require('../config/project');
 
 const watch = watchMode();
 
@@ -10,7 +12,8 @@ module.exports = (plugins, options) => {
   return plugins.reduce((promise, parallel) => {
     return promise.then(() => {
       return Promise.all(parallel.map(task => {
-        return require(task)({log, logIf, logIfP, watch, base, statics, inTeamCity})(options)
+        const taskFn = isString(task) ? require(task) : task;
+        return taskFn({log, logIf, logIfP, watch, base, statics, inTeamCity, projectConfig})(options)
           .catch(error => {
             logIfAny(error);
             if (!watch) {
