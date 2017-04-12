c1dd29a766405c221bc5abbe280c9b3aeb71846b
Ronen Amiel <ronena@wix.com>
2017-04-12 11:26:27 +0300

    yoshi-jest (#74)
    * yoshi-jest
    
    * remove redundant code

---
+++ plugins/yoshi-jest/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('path');
+const jestCli = require('jest-cli');
+
+module.exports = ({log, watch, inTeamCity, projectConfig}) => {
+  function jest() {
+    const config = projectConfig.jestConfig();
+
+    if (inTeamCity()) {
+      config.testResultsProcessor = path.join(__dirname, 'node_modules', 'jest-teamcity-reporter');
+    }
+
+    return new Promise((resolve, reject) => {
+      jestCli.runCLI({watch, config}, process.cwd(), result => {
+        result.success ? resolve() : reject('jest failed');
+      });
+    });
+  }
+
+  return log(jest);
+};
