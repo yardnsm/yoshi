5ccb0c377815a5487db8573822c016e003858145
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-19 09:28:19 +0300

    remove protractor/selenium magic form web generator

--- generators/generator-yoshi-web/generators/react/templates/protractor.conf.js
+++ generators/generator-yoshi-web/generators/react/templates/protractor.conf.js
@@ -1,6 +1,8 @@
 import {start} from './test/fakes/fake-server';
 
 export const config = {
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['test/**/*.e2e.js'],
   baseUrl: 'http://localhost:3100/',
   onPrepare() {
     browser.ignoreSynchronization = true;
