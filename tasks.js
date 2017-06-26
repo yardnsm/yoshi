57a3e066bcd6c85c8f0fe6cb7e8f2e51fdf36bbd
Vilius Lukošius <vilius@wix.com>
2017-06-26 11:37:37 +0300

    Add new octopus with custom pullreq ci job (#117)
    * Added new octo
    
    * Fixed tasks.js
    
    * Additional tuning
    
    * Reverted change
    
    * removing pre-push hook functions
    
    * run yarn in travis
    
    * rm npm cache clear task for ci
    
    * pull all to have master
    
    * tuning travis
    
    * Adding git pull
    
    * do not build branches
    
    * minor cleanup
    
    * run tests separately
    
    * Minor updates to travis config

---
+++ tasks.js
@@ -0,0 +1,100 @@
+const Start = require('start').default,
+  reporter = require('octopus-start-reporter'),
+  startTasks = require('octopus-start-tasks'),
+  startModulesTasks = require('octopus-start-modules-tasks'),
+  prepush = require('octopus-start-preset-prepush'),
+  dependencies = require('octopus-start-preset-dependencies'),
+  modules = require('octopus-start-preset-modules'),
+  idea = require('octopus-start-preset-idea'),
+  depcheck = require('octopus-start-preset-depcheck');
+
+const start = Start(reporter());
+
+module.exports['modules:sync'] = () => start(modules.sync());
+module.exports['deps:sync'] = () => start(dependencies.sync());
+module.exports['deps:extraneous'] = () => start(dependencies.extraneous());
+module.exports['deps:unmanaged'] = () => start(dependencies.unmanaged());
+module.exports['deps:latest'] = () => start(dependencies.latest());
+module.exports['idea'] = () => start(idea());
+module.exports['init'] = () => start(prepush());
+module.exports['depcheck'] = () => start(depcheck({ignoreMatches: ['wnpm-ci']}));
+
+
+module.exports.sync = () => start(
+  /*modules.sync(),
+   /*dependencies.sync(),
+   dependencies.unmanaged(),
+   dependencies.latest()*/
+  dependencies.extraneous()
+)
+
+/* links, installs, builds all modules */
+module.exports.bootstrap = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.modules.removeUnchanged('bootstrap'),
+  startModulesTasks.iter.forEach()((module, input, asyncReporter) => Start(asyncReporter)(
+    startTasks.ifTrue(module.dependencies.length > 0)(() =>
+      Start(asyncReporter)(startModulesTasks.module.exec(module)(`yarn link ${module.dependencies.map(item => item.name).join(' ')}`))
+    ),
+    startModulesTasks.module.exec(module)('yarn --no-lockfile && yarn link'),
+    startModulesTasks.module.exec(module)('yarn run build'),
+    startModulesTasks.module.markBuilt(module, 'bootstrap')
+  ))
+)
+
+/* run tests for changed modules */
+module.exports.test = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.modules.removeUnchanged('test'),
+  startModulesTasks.iter.forEach()(module => start(
+    startModulesTasks.module.exec(module)('yarn run test'),
+    startModulesTasks.module.markBuilt(module, 'test')
+  ))
+)
+
+/* run linter for changed modules */
+module.exports.lint = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.modules.removeUnchanged('lint'),
+  startModulesTasks.iter.forEach()(module => start(
+    startModulesTasks.module.exec(module)('yarn run lint'),
+    startModulesTasks.module.markBuilt(module, 'lint')
+  ))
+)
+
+
+/* unbuild all modules */
+module.exports.unbuild = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.iter.async()((module, input, asyncReporter) => Start(asyncReporter)(
+    startModulesTasks.module.markUnbuilt(module, 'bootstrap'),
+    startModulesTasks.module.markUnbuilt(module, 'test'),
+    startModulesTasks.module.markUnbuilt(module, 'lint')
+  ))
+)
+
+/* clean all modules */
+module.exports.clean = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.iter.async()((module, input, asyncReporter) => Start(asyncReporter)(
+    startModulesTasks.module.exec(module)('rm -rf node_modules && rm -rf target')
+    )
+  )
+)
+
+/* run job in pullreq ci - build only modules that have changes since origin/master */
+module.exports.pullreq = () => start(
+  startModulesTasks.modules.load(),
+  startModulesTasks.modules.removeGitUnchanged('origin/master'),
+  startModulesTasks.modules.removeExtraneousDependencies(),
+  startModulesTasks.iter.forEach()((module, input, asyncReporter) => start(
+    startTasks.ifTrue(module.dependencies.length > 0)(() =>
+      Start(asyncReporter)(startModulesTasks.module.exec(module)(`yarn link ${module.dependencies.map(item => item.name).join(' ')}`))
+    ),
+    startModulesTasks.module.exec(module)('yarn --no-lockfile && yarn link'),
+    startModulesTasks.module.exec(module)('yarn run build')
+  )),
+  startModulesTasks.iter.forEach()((module, input, asyncReporter) => start(
+    startModulesTasks.module.exec(module)('yarn run test')
+  ))
+)
