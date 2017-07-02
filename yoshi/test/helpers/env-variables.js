1aa01dfe592bd64de2e93298c58dadb5700d83bb
Jonathan Adler <jonathanadler2@gmail.com>
2017-07-02 16:10:50 +0300

    yoshi: inject artifact version in build time (#146)

--- yoshi/test/helpers/env-variables.js
+++ yoshi/test/helpers/env-variables.js
@@ -3,5 +3,6 @@
 module.exports = {
   outsideTeamCity: {TEAMCITY_VERSION: '', BUILD_NUMBER: ''},
   insideTeamCity: {TEAMCITY_VERSION: 1},
-  insideWatchMode: {WIX_NODE_BUILD_WATCH_MODE: true}
+  insideWatchMode: {WIX_NODE_BUILD_WATCH_MODE: true},
+  teamCityArtifactVersion: {ARTIFACT_VERSION: '1.0.0'}
 };
