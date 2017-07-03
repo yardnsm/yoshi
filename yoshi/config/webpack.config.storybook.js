78713e4e7b2556be556eb82aeebe1aebfefebfbe
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-07-03 10:25:43 +0300

    Remove process.env.STORYBOOK3 support

--- yoshi/config/webpack.config.storybook.js
+++ yoshi/config/webpack.config.storybook.js
@@ -8,33 +8,11 @@ module.exports = config => {
 
   config.resolve.extensions = union(config.resolve.extensions, webpackCommonConfig.resolve.extensions);
 
-  config.module.loaders = process.env.STORYBOOK3 ?
-    getStorybook3Loaders() :
-    getStorybook2Loaders();
-
-  return config;
-};
-
-function getStorybook3Loaders() {
-  return [
-    webpackCommonConfig.module.rules,
+  config.module.loaders = [
+    ...webpackCommonConfig.module.rules,
     require('../lib/loaders/json')(),
     require('../lib/loaders/sass')(true, cssModules, false).client
   ];
-}
 
-function getStorybook2Loaders() {
-  return [
-    webpackCommonConfig.module.rules.map(transformWp2LoadersToWp1),
-    require('../lib/loaders/json')(),
-    require('../lib/loaders/sass.storybook')(cssModules)
-  ];
-}
-
-function transformWp2LoadersToWp1(loader) {
-  loader.loaders = loader.use;
-  loader.query = loader.options;
-  delete loader.use;
-  delete loader.options;
-  return loader;
-}
+  return config;
+};
